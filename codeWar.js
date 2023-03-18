d22c87a0bace70fffb9e7973c6718080027dda16
SergeMaikere <serge.maikere@gmail.com>
2023-03-18 15:02:38 +0100

    Pass all test

--- codeWar.js
+++ codeWar.js
@@ -1,21 +1,24 @@
 const grid = [ 
-    [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
-    [ ' ', ' ', ' ', '+', '-', '-', '-', '-', '-', '-', '-', '+', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
-    [ ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', '+', '+', '+', '-', '-', '-', '+', ' ', ' ', ' ', ' ', ' ' ], 
-    [ 'X', '-', '-', '+', ' ', ' ', ' ', ' ', ' ', ' ', '+', '-', '+', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ' ] 
+    [ ' ', ' ', ' ', '+', '-', '-', '-', '-', '-', '+', ' ', ' ' ],
+    [ ' ', ' ', ' ', '|', '+', '-', '-', '-', '+', '|', ' ', ' ' ],
+    [ ' ', ' ', ' ', '|', '|', '+', '-', '+', '|', '|', ' ', ' ' ],
+    [ ' ', ' ', ' ', '|', '|', '|', 'X', '+', '|', '|', ' ', ' ' ],
+    [ ' ', ' ', ' ', 'X', '|', '+', '-', '-', '+', '|', ' ', ' ' ],
+    [ ' ', ' ', ' ', ' ', '+', '-', '-', '-', '-', '+', ' ', ' ' ] 
 ];
 
 const isNorth = (grid, pos) => pos.row !== 0 && !isHyphen(grid, pos) && /X|\||\+/.test(grid[pos.row - 1][pos.col]);
-const isEast = (grid, pos) => pos.col !== grid[0].length - 1 && /X|\+|\-/.test(grid[pos.row][pos.col + 1]);
+const isEast = (grid, pos) => pos.col !== grid[0].length - 1 && !isLadder(grid, pos) && /X|\+|\-/.test(grid[pos.row][pos.col + 1]);
 const isSouth = (grid, pos) => pos.row !== grid.length - 1 && !isHyphen(grid, pos) && /X|\||\+/.test(grid[pos.row + 1][pos.col]);
-const isWest = (grid, pos) => pos.col !== 0 && /X|\+|\-/.test(grid[pos.row][pos.col - 1]);
+const isWest = (grid, pos) => pos.col !== 0 && !isLadder(grid, pos) && /X|\+|\-/.test(grid[pos.row][pos.col - 1]);
 
 const isPlus = (grid, pos) => /\+/.test(grid[pos.row][pos.col]);
 const isHyphen = (grid, pos) => /\-/.test(grid[pos.row][pos.col]);
+const isLadder = (grid, pos) => /\|/.test(grid[pos.row][pos.col]);
 
 const isEmptyRow = row => row.every( char => /^[\s]$/.test(char) );
 
-const isThisTheEnd = (end, pos) => pos.row === end.row && pos.col === end.col;
+const isThisTheEnd = (end, pos) => end === pos;
 
 const isNewPosition = (positions, newPos) => positions.every(oldPos => oldPos !== newPos);
 
@@ -28,6 +31,15 @@ const isWrongTurn = (grid, pos, newPos, oldPos, direction) => {
     if (direction === 'west') return oldPos.col === pos.col + 1;
 }
 
+const isAllCharUsed = (grid, parcours) => {
+    return [...grid]
+    .map( (row, i) => isEmptyRow(row) ? [] : getAllPositions(row, i) )
+    .reduce( (acc, arr) => acc.concat(arr), [] )
+    .filter( pos => pos )
+    .every( pos => parcours.includes(pos) );
+}
+
+const getAllPositions = (row, i) => row.map( (char, j) => char !== ' ' ? `${i}.${j}` : null );
 
 const getStartPosition = (row, i) => {
     if (!/X/.test([...row].join())) return [];
@@ -62,41 +74,34 @@ const findPath = (grid, parcours) => {
     const pos = parsePos(parcours[parcours.length - 1]);
     const oldPos = parsePos(parcours[parcours.length - 2]);
     const compas = getNext(pos);
+    let nextStep = [];
 
     if (isNorth(grid, pos) && isNewPosition(parcours, simplifyPos(compas.north)) && !isWrongTurn(grid, pos, compas.north, oldPos, 'north')) 
-        return simplifyPos(compas.north);
+        nextStep.push(simplifyPos(compas.north));
     if (isEast(grid, pos) && isNewPosition(parcours, simplifyPos(compas.east)) && !isWrongTurn(grid, pos, compas.east, oldPos, 'east')) 
-        return simplifyPos(compas.east);
+        nextStep.push(simplifyPos(compas.east));
     if (isSouth(grid, pos) && isNewPosition(parcours, simplifyPos(compas.south)) && !isWrongTurn(grid, pos, compas.south, oldPos, 'south')) 
-        return simplifyPos(compas.south);
+        nextStep.push(simplifyPos(compas.south));
     if (isWest(grid, pos) && isNewPosition(parcours, simplifyPos(compas.west)) && !isWrongTurn(grid, pos, compas.west, oldPos, 'west')) 
-        return simplifyPos(compas.west);
-
-    console.log('The line is faulty');
-    return false;
+        nextStep.push(simplifyPos(compas.west));
+    
+    return nextStep.length !== 1 ? false : nextStep[0];
 }
 
 const isParcourValide = (grid, end, parcours) => {
-    const pos = parsePos(parcours[parcours.length - 1]);
-    console.log({pos})
-    if (!pos) return false;
-    if (isThisTheEnd(end, pos)) return true;
+    if (!parcours[parcours.length - 1]) return false;
+    if (isThisTheEnd(end, parcours[parcours.length - 1])) return isAllCharUsed(grid, parcours);
 
     const curr = findPath(grid, parcours);
-    console.log({end, parcours, curr});
     return isParcourValide(grid, end, [...parcours, curr])
 }
 
 const line = grid => {
-    console.log({grid})
     const start = [...grid]
     .map( (row, i) => isEmptyRow(row) ? [] : getStartPosition(row, i) )
     .reduce( (acc, arr) => acc.concat(arr), [] )
 
-    console.log({start});
     return start.length !== 2 ? false : 
         ( !isParcourValide(grid, start[1], [start[0]]) ? isParcourValide(grid, start[0], ([start[1]])) : true );
 }
-
-console.log(line(grid));
 
