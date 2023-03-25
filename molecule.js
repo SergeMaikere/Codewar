589edfb84fa487b3c4fb15d9d4bba9e9b8652dee
SergeMaikere <serge.maikere@gmail.com>
2023-03-25 23:12:28 +0100

    refactoring classes + closer method

--- molecule.js
+++ molecule.js
@@ -9,19 +9,23 @@ class Exception extends Error {
 }
 
 class Helper {
-    static link = (atom1, atom2) => {
-        atom1.linkTo(atom2);
-        atom2.linkTo(atom1);
-    }
 
     static pipe = (...fns) => args => fns.reduce((f, g) => g(f), args);
+
+    static getChain = (br, arr) => arr[br - 1];
+    static getLastChain = arr => arr[arr.length - 1];
+
+    static getAtom = (a, br, arr) => Helper.getChain(br, arr).getAtom(a);
+    static getLastAtom = (br, arr) => Helper.getChain(br, arr).getLastAtom();
 }
 
 class Molecule {
     constructor (name = "") {
         this.name = name;
         this.branches = [];
-        this.atoms = {'C': []};
+        this.chains = [];
+        this.atoms = {};
+        this.locked = false;
     }
 
     get name () { return this._name };
@@ -31,116 +35,181 @@ class Molecule {
     get atoms () { return this._atoms };
     set atoms (newValue) { this._atoms = newValue };
 
-    #addNewBrancheIds = () => this.atoms.C.concat(this.branches[this.branches.length - 1][0].getAllIds());
-
     #addNewBranche = length => {
-        const atoms = this.atoms.C;
-        this.branches.push([new Branche(length, this.atoms.C[this.atoms.C.length -1])]);
-        this.atoms.C = this.#addNewBrancheIds();
-
+        this.branches.push( new Branche([...Array(length)].map( el => 'C'), {...this.atoms}) );
+        this.atoms = Helper.getLastChain(this.branches).currAtoms;
     }
 
-    #addAtomToAtoms = (elt, id) =>  {
-        if (this.atoms[elt]) this.atoms[elt].push(id);
-        if (!this.atoms[elt]) this.atoms[elt] = [ id ];
+    #addNewChain = arrOfElt => this.chains.push( new Chain(arrOfElt, {...this.atoms}) );
+
+    #replaceAtomInAtoms = (c, elt, id) => {
+        this.atoms.C = this.atoms.C.filter(carbonID => carbonID !== id);
+        this.addAtomToAtoms(elt, id, this.atoms);
     }
 
-    #replaceAtomInAtoms = (c, br, elt, id) => {
-        this.atoms['C'] = this.atoms['C'].filter(carbonID => carbonID !== id);
-        this.#addAtomToAtoms(elt, id);
+    #updateAtoms = ch => this.atoms = ch.currAtoms;
+
+    #checkMoleculeIsLocked = () => {
+        if(this.locked) throw new Exception('LockedMolecule');
     }
 
-    #getNextIdOfElt = elt => this.atoms?.[elt]?.length + 1 || 1;
+    #hydrate = arrOfChains => {
+        arrOfChains.forEach(
+            ch => {
+                ch.addHydrogens(this.atoms);
+                this.#updateAtoms(ch);
+            }
+        )
+    }
 
-    #setArrayOfAtoms = arrOfElt => arrOfElt.map( elt => new Atom(elt, this.#getNextIdOfElt(elt)) );
+    addAtomToAtoms = (elt, id, index) => index[elt] ? index[elt].push(id) : index[elt] = [ id ];
 
-    #linkArrayOfAtoms = arr => {
-        [...Array(arr.length)].forEach( (a, i) => i < arr.length - 1 && Helper.link(arr[i], arr[i + 1]) );
-        return arr;
+    link = (atom1, atom2) => {
+        atom1.linkTo(atom2);
+        atom2.linkTo(atom1);
     }
 
     brancher = (...n) => {
+        this.#checkMoleculeIsLocked();
+
         n.forEach( br => this.#addNewBranche(br));
         return this;
     }
 
     bounder = (...arrays) => {
+        this.#checkMoleculeIsLocked();
+
         arrays.forEach(
             array => {
                 const [a1, br1, a2, br2] = array;
-                Helper.link(this.branches[br1 - 1][0].carbons[a1 - 1], this.branches[br2 - 1][0].carbons[a2 - 1])
+                this.link(Helper.getAtom(a1, br1, this.branches), Helper.getAtom(a2, br2, this.branches))
             }
         );
         return this;
     }
 
     mutate = (...arrays) => {
+        this.#checkMoleculeIsLocked();
+
         arrays.forEach(
             array => {
                 const [ c, br, elt ] = array;
-                this.branches[br - 1][0].carbons[c - 1] = new Atom(elt, this.branches[br - 1][0].carbons[c - 1].id);
-                this.#replaceAtomInAtoms(c, br, elt, this.branches[br - 1][0].carbons[c - 1].id)
+                Helper.getChain(br, this.branches).mutate(c, elt);
+                this.#replaceAtomInAtoms(c, elt, Helper.getAtom(c, br, this.branches).id)
             }
         )
         return this;
     }
 
     add = (...arrays) => {
+        this.#checkMoleculeIsLocked();
+
         arrays.forEach(
             array => {
                 const [ c, br, elt ] = array;
-                const id = this.#getNextIdOfElt(elt);
-                Helper.link(this.branches[br - 1][0].carbons[c - 1], new Atom(elt, id));
-                this.#addAtomToAtoms(elt, id);
+                Helper.getChain(br, this.branches).add(c, br, elt, this.atoms)
+                this.addAtomToAtoms(elt, Helper.getAtom(c, br, this.branches).id, this.atoms);
             }
         )
         return this;
     }
 
     addChaining = (...args) => {
-        const [ c, br ] = args.splice(0, 2);
+        this.#checkMoleculeIsLocked();
 
-        const chain = Helper.pipe(
-            this.#setArrayOfAtoms,
-            this.#linkArrayOfAtoms,
-            arr => arr.map( a => {this.#addAtomToAtoms(a.element, a.id); return a;} )
-        )(args)
-        Helper.link(this.branches[br - 1][0].carbons[c - 1], chain[0]);
-        console.log(chain)
+        const [ c, br ] = args.splice(0, 2);
+        this.#addNewChain( args );
+        this.link(Helper.getAtom(c, br, this.branches), Helper.getLastChain(this.chains).atoms[0]);
+        this.#updateAtoms( Helper.getLastChain(this.chains) )
+        return this;
+    }
+
+    closer = () => {
+        this.#checkMoleculeIsLocked();
+
+        this.#hydrate(this.branches);
+        this.#hydrate(this.chains);
+        this.locked = true;
     }
 }
 
-class Branche {
-    constructor (length, _lastId = 0) {
-        this.length = length;
-        this.lastId = _lastId;
-        this.carbons = this.#setCarbons();
+
+class Chain extends Molecule {
+
+    #arrOfElt;
+
+    constructor(_arrOfElt, _currAtoms, name = "") {
+        super(name);
+        this.#arrOfElt = _arrOfElt;
+        this.currAtoms = _currAtoms;
+        this.atoms = this.setAtoms();
     }
 
-    get length () { return this._length };
-    set length (newValue) { this._length = newValue };
-    get lastId () { return this._lastId };
-    set lastId (newValue) { this._lastId = newValue };
-    get carbons () { return this._carbons };
-    set carbons (newValue) { this._carbons = newValue };
+    get currAtoms () { return this._currAtoms };
+    set currAtoms (newValue) { this._currAtoms = newValue };
+    get atoms () { return this._atoms };
+    set atoms (newValue) { this._atoms = newValue };
 
-    #setCarbons = () => Helper.pipe(
-        this.#setArrayOfCarbons,
-        this.#linkCarbons
-    )([...Array(this.length)]);
+    getAtom = a => this.atoms[a - 1];
 
-    #setArrayOfCarbons = arr => arr
-    .map( (a, i) => this.lastId + i + 1 )
-    .map( id => new Atom('C', id) );
-    
-    #linkCarbons = arr => {
-        [...Array(this.length)].forEach( (a, i) => i < this.length - 1 && Helper.link(arr[i], arr[i + 1]) );
+    getLastAtom = () => this.atoms[this.atoms.length - 1];
+
+    setArrayOfAtoms = arrOfElt => arrOfElt.map( elt => this.#setNewAtom(elt) );
+
+    #setNewAtom = elt => {
+        const newId = this.getNextIdOfElt(elt); 
+        this.addAtomToAtoms(elt, newId, this.currAtoms);
+        return new Atom(elt, newId);
+    }
+
+    getNextIdOfElt = elt => this.currAtoms?.[elt]?.[this.currAtoms?.[elt]?.length - 1] + 1 || 1 ;
+
+    linkAtoms = arr => {
+        [...Array(arr.length)].forEach( (a, i) => i < arr.length - 1 && this.link(arr[i], arr[i + 1]) );
         return arr;
     }
 
-    getAllIds = () => this.carbons.map(c => c.id);
+    setAtoms = () =>{ 
+        return Helper.pipe(
+            this.setArrayOfAtoms,
+            this.linkAtoms
+        )([...this.#arrOfElt]);
+    }
 
-    toString = () => `Branche of length ${this.length}, carbons : ${this.carbons.map(c => `${c.element}.${c.id}`).join(',')}`;
+    addHydrogens = molAtoms => {
+        this.currAtoms = molAtoms;
+
+        this.atoms.forEach( 
+            a =>{ 
+                [...Array(a.getFreeSpots())]
+                .map( spot => this.#setNewAtom('H') )
+                .map( h => { this.link(a, h); return h; } )
+                .forEach( a => this.atoms.push(a) )
+            }
+        )
+    }
+
+        
+    toString = () => `Branche of length ${this.#arrOfElt.length}, elements : ${this.atoms.map(a => `${a.element}.${a.id}`).join(',')}`;
+}
+
+
+
+
+class Branche extends Chain {
+    constructor (arrOfElt, currAtoms) {
+        super(arrOfElt, currAtoms);
+    }
+
+    mutate = (c, elt) => {
+        this.atoms[c - 1] = new Atom(elt, this.atoms[c - 1].id);
+        this.linkAtoms([this.atoms[c - 2], this.atoms[c - 1], this.atoms[ c ]]);
+    }
+
+    add = (c, br, elt) => {
+        this.link(this.atoms[c - 1], new Atom(elt, this.getNextIdOfElt(elt)));
+    } 
+
 }
 
 
@@ -152,6 +221,17 @@ class Atom {
         this.weight = Atom.VALID_ATOMS[this.element].weight;
         this.linkedTo = {};
     }
+
+    get element () { return this._element }
+    set element (newValue) { this._element = newValue }
+    get id () { return this._id }
+    set id (newValue) { this._id = newValue }
+    get valence () { return this._valence }
+    set valence (newValue) { this._valence = newValue }
+    get weight () { return this._weight }
+    set weight (newValue) { this._weight = newValue }
+    get linkedTo () { return this._linkedTo }
+    set linkedTo (newValue) { this._linkedTo = newValue }
 
     static VALID_ATOMS = {
         'H': {valence: 1, weight: 1.0},    
@@ -169,22 +249,33 @@ class Atom {
 
 
     linkTo = atom => {
-        if (!this.isLinkLegit(atom)) throw new Exception('Linkage is against the laws of physics');
+        if (!this.#isLinkLegit(atom) || this.#isSameAtom(atom)) throw new Exception('InvalidBond');
         this.linkedTo[atom.element] ? this.linkedTo[atom.element].push(atom.id) : this.linkedTo[atom.element] = [atom.id];
     }
 
-    isLinkLegit = atom => Object.keys(this.linkedTo).length + 1 <= this.valence;
+    getFreeSpots = () => this.valence - Object.keys(this.linkedTo).length;
+
+    #isLinkLegit = atom => Object.keys(this.linkedTo).length + 1 <= this.valence;
+
+    #isSameAtom = atom => atom.id === this.id && atom.element === this.element;
 
     #getHighestIdOfElt = elt => [...this.linkedTo[elt]].sort((a,b) => a>b).pop();
 
+    #formatLinkedToElt = arrOfElt => arrOfElt.map( elt => `${elt}${this.#getHighestIdOfElt(elt)}` );
+
+    #moveHToLastPos = arrOfElt => {
+        const indexOfH = Object.keys(this.linkedTo).indexOf('H');
+        if ( indexOfH !== -1 ) myAtoms.push( myAtoms.splice(indexOfH, 1)[0] );
+    } 
+
     #formatLinkedTo = () => {
-        const myElements = Object.keys(this.linkedTo);
-        let myAtoms = myElements.map( elt => `${elt}${this.#getHighestIdOfElt(elt)}` );
+        // const myElements = Object.keys(this.linkedTo);
+        // let myAtoms = myElements.map( elt => `${elt}${this.#getHighestIdOfElt(elt)}` );
 
-        const indexOfH = myElements.indexOf('H');
-        if ( indexOfH ) myAtoms.push( myAtoms.splice(indexOfH, 1)[0] );
-
-        return myAtoms.join(',');
+        // const indexOfH = myElements.indexOf('H');
+        // if ( indexOfH ) myAtoms.push( myAtoms.splice(indexOfH, 1)[0] );
+        return Helper.pipe( this.formatLinkedToElt, moveHToLastPos )(Object.keys(this.linkedTo)).join(',')
+        //return myAtoms.join(',');
     }
 
     #isElement = elt => {
@@ -198,16 +289,17 @@ class Atom {
 
 const m = new Molecule('sergium');
 console.log(m.name)
-m.brancher(5, 7, 12);
-m.bounder([2,1,3,2], [1,2,2,3]);
-m.mutate([2, 2, 'O'])
-m.add([5, 2, 'O'])
-m.addChaining(2, 3, "N", "P", "C", "Mg", "Br")
-console.log(m.branches)
-//console.log(m.branches[2][0].carbons)
+m.brancher(10);
+//m.bounder([2,1,7,1]);
+//m.mutate([6, 1, 'O'])
+//m.add([5, 1, 'O'])
+//m.addChaining(2, 1, "N", "P", "C", "Mg", "Br")
+m.closer();
 console.log(m.atoms)
 console.log(m.branches[0].toString())
-console.log(m.branches[1].toString())
-console.log(m.branches[2].toString())
+// console.log(m.branches[1].toString())
+// console.log(m.branches[2].toString())
+// console.log(m.chains[0].toString())
+//console.log(m.chains[0].atoms)
 
 
